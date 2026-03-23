8a2dd1c250dd95c299d2c96413ef44ea6a79f475
Anvarys <105349498+Anvarys@users.noreply.github.com>
2026-03-23 12:07:53 +0100

    added cURL buttons to the versions list

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-const observer = new MutationObserver(() => {
+const downloadScreenObserver = new MutationObserver(() => {
   const dialogs = document.querySelectorAll('[role="dialog"]');
 
   if (!dialogs) {return;}
@@ -21,7 +21,7 @@ const observer = new MutationObserver(() => {
 
   const curlButton = document.getElementById("ME-curl-button");
 
-  curlButton.addEventListener("click", () => {
+  curlButton.addEventListener("click", (e) => {
     const downloadButton = document.evaluate(
       "/html/body/div[1]/div[4]/main/div[3]/div[2]/div[3]/div/div[2]/div/div[3]/div/div/div/div/div[3]/a",
       document,
@@ -31,12 +31,47 @@ const observer = new MutationObserver(() => {
     ).singleNodeValue;
 
     navigator.clipboard.writeText("curl -O " + downloadButton.href);
-
-    curlButton.textContent = "Copied!"
+    e.currentTarget.textContent = "Copied!"
   })
 });
 
-observer.observe(document.body, {
+downloadScreenObserver.observe(document.body, {
+  childList: true,
+  subtree: true
+})
+
+const versionsScreenObserver = new MutationObserver(() => {
+  if (!window.location.pathname.includes("/versions")) {return; }
+
+  const parent = document.evaluate(
+    "/html/body/div[1]/div[4]/main/div[3]/div[2]/div[3]/section/div[2]",
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue;
+
+  if (!parent) {return;}
+
+  if (document.getElementById("ME-curl-button-version-0")) {return; }
+
+  const versionsCount = (parent.children.length-1)/2
+  
+  for (let i = 0; i < versionsCount; i++) {
+    downloadButton = parent.children[i*2+2].children[2].children[0]
+
+    downloadButton.insertAdjacentHTML("beforebegin", `<div data-v-71fa7669="" class="btn-wrapper text-base" style="--_bg: var(--color-brand); --_text: var(--color-accent-contrast); --_hover-bg: var(--color-brand); --_hover-text: var(--color-accent-contrast); --_box-shadow: var(--shadow-button); --_height: 2.25rem; --_width: auto; --_radius: 0.75rem; --_padding-x: calc(0.75rem - 0.125rem); --_padding-y: 0.5rem; --_gap: 0.375rem; --_font-weight: 600; --_icon-size: 1.25rem;"><button data-v-71fa7669-s="" class="min-w-0" id="ME-curl-button-version-${i}"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=""><rect width="20" height="8" x="2" y="2" rx="2" ry="2"/><rect width="20" height="8" x="2" y="14" rx="2" ry="2"/><line x1="6" x2="6.01" y1="6" y2="6"/><line x1="6" x2="6.01" y1="18" y2="18"/></svg> cURL </button></div>`);
+
+    const curlButton = document.getElementById("ME-curl-button-version-"+i);
+
+    curlButton.addEventListener("click", (e) => {
+      navigator.clipboard.writeText("curl -O " + downloadButton.children[0].href);
+      e.currentTarget.textContent = "Copied!";
+    })
+  }
+});
+
+versionsScreenObserver.observe(document.body, {
   childList: true,
   subtree: true
 })
